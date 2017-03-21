2f782efcb85e0e64a6d43f0e98b58ee9204a022e
Dibakar Mistry <dibakar.ece@gmail.com>
2017-03-22 01:49:59 +0530

    design in dashboard section and functionality implementation

--- app/src/main/java/com/shades/shade/fragments/FragmentDashboard.java
+++ app/src/main/java/com/shades/shade/fragments/FragmentDashboard.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.shades.shade.R;
 import com.shades.shade.widgets.ShadeTextView;
+import com.shades.shade.widgets.UVIndexProgress;
 
 
 public class FragmentDashboard extends Fragment implements View.OnClickListener {
@@ -22,12 +23,13 @@ public class FragmentDashboard extends Fragment implements View.OnClickListener 
     private static Context context;
 
     private enum SensorSection {
-        VIEW_HISTORY, DAILY_LIMIT
+        VIEW_HISTORY, DAILY_LIMIT, SENSOR_NOT_DETECTING
     }
 
     private RelativeLayout relativeLayout;
     private LayoutInflater inflater;
-
+    private UVIndexProgress UVIndex;
+    private ShadeTextView txt_UVIndex;
 
     public static FragmentDashboard newInstance(int position, Context mContext) {
         FragmentDashboard f = new FragmentDashboard();
@@ -54,9 +56,15 @@ public class FragmentDashboard extends Fragment implements View.OnClickListener 
     private void setupView(View v) {
         context = getActivity();
         inflater = LayoutInflater.from(context);
-        relativeLayout = (RelativeLayout) v.findViewById(R.id.frg_sensor_statusLayout);
 
+        relativeLayout = (RelativeLayout) v.findViewById(R.id.frg_sensor_statusLayout);
+        UVIndex = (UVIndexProgress) v.findViewById(R.id.frg_prg_UVIndex);
+        UVIndex = (UVIndexProgress) v.findViewById(R.id.frg_prg_UVIndex);
+        txt_UVIndex = (ShadeTextView) v.findViewById(R.id.frg_txt_UVIndex);
+
+        //Test Data
         setLayoutFor(SensorSection.DAILY_LIMIT);
+        showUVIndex(5.3f);
     }
 
     private void setLayoutFor(SensorSection layoutFor) {
@@ -71,18 +79,44 @@ public class FragmentDashboard extends Fragment implements View.OnClickListener 
             case DAILY_LIMIT:
                 loadDailyLimit(53.5f);
                 break;
+
+            case SENSOR_NOT_DETECTING:
+                loadSensorDetecting();
+                break;
         }
     }
 
+    /**
+     * Set your UV Index as per your device calculate
+     *
+     * @param uvIndex
+     */
+    private void showUVIndex(float uvIndex) {
+        if (uvIndex > 0) {
+            UVIndex.setPGIndex(uvIndex);
+            txt_UVIndex.setText("" + uvIndex);
+        } else {
+            UVIndex.setPGIndex(0);
+            txt_UVIndex.setText("0.00");
+        }
+
+    }
+
+    /**
+     * Load View History Layout
+     */
     private void loadViewHistory() {
         View view = inflater.inflate(R.layout.inflate_dashboard_viewhistory, null);
         ((ShadeTextView) view.findViewById(R.id.frg_txt_viewHistory)).setOnClickListener(this);
         relativeLayout.addView(view);
     }
 
+    /**
+     * Load View Daily Limit Layout
+     */
     private void loadDailyLimit(float value) {
         try {
-            View view = inflater.inflate(R.layout.inflate_dashboard_viewhistory, null);
+            View view = inflater.inflate(R.layout.inflate_dashboard_dailylimit, null);
             ((ImageView) view.findViewById(R.id.img_rightArrow)).setOnClickListener(this);
             ((ShadeTextView) view.findViewById(R.id.frg_txt_currentLimit)).setText("" + value);
             relativeLayout.addView(view);
@@ -90,4 +124,14 @@ public class FragmentDashboard extends Fragment implements View.OnClickListener 
             e.printStackTrace();
         }
     }
+
+    /**
+     * Load View Sensor Not Detecting Layout
+     */
+    private void loadSensorDetecting() {
+        View view = inflater.inflate(R.layout.inflate_dashboard_viewhistory, null);
+        ((ShadeTextView) view.findViewById(R.id.frg_txt_viewHistory)).setOnClickListener(this);
+        relativeLayout.addView(view);
+    }
+
 }
