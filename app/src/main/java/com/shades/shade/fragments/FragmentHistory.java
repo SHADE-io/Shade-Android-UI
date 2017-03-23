4e452f3bed949f009498f28e4a63b0a05ad7766e
Dibakar Mistry <dibakar.ece@gmail.com>
2017-03-24 01:36:41 +0530

    design in History section and functionality implementation

--- app/src/main/java/com/shades/shade/fragments/FragmentHistory.java
+++ app/src/main/java/com/shades/shade/fragments/FragmentHistory.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import com.shades.shade.R;
 import com.shades.shade.widgets.ShadeTextView;
