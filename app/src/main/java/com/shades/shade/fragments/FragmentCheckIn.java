34caa75797ddb1602f8d5706067d6cac01d5fb29
Dibakar Mistry <dibakar.ece@gmail.com>
2017-03-24 23:20:07 +0530

    design in History/CheckIn section and functionality implementation

--- app/src/main/java/com/shades/shade/fragments/FragmentCheckIn.java
+++ app/src/main/java/com/shades/shade/fragments/FragmentCheckIn.java
@@ -7,13 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.shades.shade.R;
+import com.shades.shade.widgets.ShadeEditText;
+import com.shades.shade.widgets.ShadeTextView;
+
+import java.util.ArrayList;
 
 public class FragmentCheckIn extends Fragment {
 
     private static final String TAG = "FragmentCheckIn";
     private static Context context;
+    private String[] state = {"High", "Medium", "Normal", "Low", "Very Low"};
+    private ArrayList<ImageView> listSmiley = new ArrayList<>();
+    private ShadeEditText edtFelling;
+    private ShadeTextView smileyState;
 
 
     public static FragmentCheckIn newInstance(int position, Context mContext) {
@@ -26,9 +35,41 @@ public class FragmentCheckIn extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_checkin, null);
         setupView(view);
+        initSmileyClickListener();
         return view;
     }
 
     private void setupView(View v) {
+        smileyState = (ShadeTextView) v.findViewById(R.id.frg_checkIn_smileyState);
+        edtFelling = (ShadeEditText) v.findViewById(R.id.frg_checkIn_edtFelling);
+        listSmiley.add((ImageView) v.findViewById(R.id.frg_checkIn_imgSmiley1));
+        listSmiley.add((ImageView) v.findViewById(R.id.frg_checkIn_imgSmiley2));
+        listSmiley.add((ImageView) v.findViewById(R.id.frg_checkIn_imgSmiley3));
+        listSmiley.add((ImageView) v.findViewById(R.id.frg_checkIn_imgSmiley4));
+        listSmiley.add((ImageView) v.findViewById(R.id.frg_checkIn_imgSmiley5));
+    }
+
+    private void initSmileyClickListener() {
+        for (int i = 0; i < listSmiley.size(); i++) {
+            listSmiley.get(i).setTag(i);
+            listSmiley.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int TapIndex = Integer.valueOf(String.valueOf(view.getTag()));
+                    selectSmiley(TapIndex);
+                }
+            });
+        }
+    }
+
+    private void selectSmiley(int index) {
+        for (int i = 0; i < listSmiley.size(); i++) {
+            listSmiley.get(i).setSelected((index == i) ? true : false);
+            setState(state[i]);
+        }
+    }
+
+    private void setState(String state) {
+        smileyState.setText(state);
     }
 }
