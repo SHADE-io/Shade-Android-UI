0305a02269f01a60a965e9b7a4afc1f13f3a8849
Dibakar Mistry <dibakar.ece@gmail.com>
2017-03-26 20:47:38 +0530

    design in History/CheckIn section and functionality implementation

---
+++ app/src/main/java/com/shades/shade/utility/ShadePref.java
@@ -0,0 +1,25 @@
+package com.shades.shade.utility;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class ShadePref {
+
+    public static final String SMILEY_SELECTED = "smiley_selected";
+
+
+    public static SharedPreferences getInstance(Context context) {
+        return context.getSharedPreferences("shadePref", Context.MODE_PRIVATE);
+    }
+
+    private static void setSmileySelection(Context context, int selectedIndex) {
+        SharedPreferences.Editor editor = getInstance(context).edit();
+        editor.putInt(SMILEY_SELECTED, selectedIndex);
+        editor.commit();
+    }
+
+    private static int getSmileySelection(Context context) {
+        return getInstance(context).getInt(SMILEY_SELECTED, -1);
+    }
+}
